bf22261b6a3ad219337ba1fbb0bf13c524d3772b
Yhonatan Jesus <yhonatan@pop-os.localdomain>
2019-12-24 00:19:10 -0300

    Problem 01

---
+++ advent_code01/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() 
+{    
+    int result = 0;
+    std::ifstream myfile("../input.txt",std::fstream::in);
+    if (myfile.is_open() != true)
+    {
+        std::cout << "Error for load File" << std::endl;        
+        return -1;
+    }
+    std::string _string((std::istreambuf_iterator<char>(myfile)), std::istreambuf_iterator<char>());
+
+    for(char& a :_string)
+    {
+        (a == '(') ? result++ : result--;
+    }    
+    std::cout << result << std::endl;
+
+    return 0;
+}
