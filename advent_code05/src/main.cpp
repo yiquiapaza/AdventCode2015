2dd2b16992c1aeddda9f422ebd7c1f6045199d2e
Yhonatan Jesus <yiquiapaza@gmail.com>
2020-04-15 01:50:11 -0300

    Day 5 part 1

--- advent_code05/src/main.cpp
+++ advent_code05/src/main.cpp
@@ -2,27 +2,38 @@
 #include <fstream>
 #include <string>
 #include <algorithm>
+#include <vector>
 
 bool vowels(std::string &str);
 bool not_contain(std::string &str);
 bool twice(std::string &str);
+std::vector<std::string> read_line(std::ifstream &file);
 
 int main()
 {
   std::ifstream myFile("../input.txt", std::fstream::in);
+  int result = 0; 
   if (!myFile.is_open())
   {
     std::cout << "Error load file" << std::endl;
     return -1;
   }
-  std::string _string((std::istreambuf_iterator<char>(myFile)), std::istreambuf_iterator<char>());
+/*   std::string _string((std::istreambuf_iterator<char>(myFile)), std::istreambuf_iterator<char>());
+ */
 
-  std::string test = "jchzalrnumimnmhp";
-
-  std::cout << vowels(test) << std::endl;
-  std::cout << twice(test) << std::endl;
-  std::cout << not_contain(test) << std::endl;
-  
+  for (std::string item : read_line(myFile))
+  {
+    if( vowels(item) && twice(item) && not_contain(item))
+    {
+      std::cout << item << std::endl;
+      std::cout << "Vowels Test: " << vowels(item) << std::endl;
+      std::cout << "Twice letter: " << twice(item) << std::endl;
+      std::cout << "Not contain word: " << not_contain(item) << std::endl;
+      std::cout << "@@@@@@@@@@@@@@@@@@@@@@@" << std::endl;
+      result++;
+    }    
+  }    
+  std::cout << result << std::endl;
   /* std::cout << _string << std::endl; */
 
   return 0;
@@ -53,13 +64,24 @@ bool not_contain(std::string &str)
 bool twice(std::string &str)
 {
   bool tmp = false;
-  for(char& c:str)
+  for(int i = 0; str.length() > i ; i++)
   {
-    if(str.find(c+c) != std::string::npos)
+    if(str[i] == str[i+1])
     {
       tmp = true;
       break;
     }
   }
   return tmp;
+}
+
+std::vector<std::string> read_line(std::ifstream &file)
+{
+  std::vector<std::string> out;
+  std::string item = "";
+  while (std::getline(file,item))
+  {
+    out.push_back(item);
+  }
+  return out;
 }
